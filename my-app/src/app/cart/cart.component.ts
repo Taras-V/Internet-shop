65cf3490207bfa0e7461153eb28a7a64cd9aae07
Taras-V <vankotaras.95@gmail.com>
2019-07-26 18:03:09 +0300

    Fix conflicts

--- my-app/src/app/cart/cart.component.ts
+++ my-app/src/app/cart/cart.component.ts
@@ -18,11 +18,7 @@ export class CartComponent implements OnInit {
   
   count = 1;
   priceTogether = 35;
-<<<<<<< HEAD
-  price = 30
-=======
   price = 30;
->>>>>>> 63314c0ab0cef10ff682289b8ea3478c4b052782
 
   minusElement(){
   this.count --
